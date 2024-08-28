f9db7cf4544c0ebf041d9eeec2a1da6b851d836e
靓仔 <xuguangzhao123@outlook.com>
2024-08-28 15:40:36 +0800

    文件异常处理

--- main.py
+++ main.py
@@ -290,6 +290,7 @@ class ProgressApp:
             if not a:
                 logging.error(f"文件错误: {del_file}")
                 self.j += 1
+                self.aaa.update_progress(self.j)
                 ki = subprocess.run(["taskkill", "/F", "/IM", "ffmpeg.exe"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
                 #os.system("taskkill /F /IM ffmpeg.exe")
                 os.unlink(new_file)
